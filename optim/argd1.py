9ee5194af32c29448d0334011ba3582e5454a1bf
Titus Pinta <titus.pinta@gmail.com>
2019-02-16 04:50:09 +0200

    ARDG1 works

--- optim/argd1.py
+++ optim/argd1.py
@@ -16,7 +16,7 @@ class ARGD1(Optimizer):
         for group in self.param_groups:
             for p in group['params']:
                 self.state[p]['x_{n-1}'] = p.data
-
+                self.state[p]['nabla(z_{n})'] = None
 
 
     def __setstate__(self, state):
@@ -30,21 +30,34 @@ class ARGD1(Optimizer):
             for p in group['params']:
                 if p.grad is None:
                     continue
-
                 state = self.state[p]
+
                 d_p = p.grad.data
-#                p.data.sub_(d_p.mul(group['lr'] + group['lr'] ** 2))
-                p.data.sub_(d_p.mul(group['lr']))
+                p.data.sub_(d_p.mul(group['lr'] + group['lr'] ** 2))
 
         closure()
 
-#        for group in self.param_groups:
-#
-#            for p in group['params']:
-#                if p.grad is None:
-#                    continue
-#                d_p = p.grad.data
-#                y = p.data - beta * (p.data - ) 
+        for group in self.param_groups:
 
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                state = self.state[p]
 
+                d_p = p.grad.data
+                state['nabla(z_{n})'] = d_p
+                p.data.add_(p.data.sub(state['x_{n-1}']).mul(self.state['n_iter'] /
+                                                             (self.state['n_iter'] + 3)))
+
+        closure()
+
+        for group in self.param_groups:
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                state = self.state[p]
+
+                d_p = p.grad.data
+                p.data.add_(d_p.mul(-group['lr']).add(state['nabla(z_{n})'].mul(-group['lr'] ** 2)))
         return closure
