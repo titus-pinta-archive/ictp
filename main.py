5013974c25bea0f030ac695ad4f3689471c0df55
Titus Pinta <titus.pinta@gmail.com>
2019-02-16 07:44:07 +0200

    clean_up

--- main.py
+++ main.py
@@ -1,76 +1,141 @@
-#!/usr/bin/env python
+#! /bin/python
+
 from __future__ import print_function
-from itertools import count
-import optim
+import argparse
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
+import optim
+from torchvision import datasets, transforms
 
-W_target = torch.Tensor([[1], [2], [3], [4]])
-b_target = torch.Tensor([0.3])
+import dill
 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
 
-def make_features(x):
-    """Builds features i.e. a matrix with columns [x, x^2, x^3, x^4]."""
-    x = x.unsqueeze(1)
-    return torch.cat([x ** i for i in range(1, 4 + 1)], 1)
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
 
-
-def f(x):
-    """Approximated function."""
-    return x.mm(W_target) + b_target.item()
-
-
-def poly_desc(W, b):
-    """Creates a string description of a polynomial."""
-    result = 'y = '
-    for i, w in enumerate(W):
-        result += '{:+.2f} x^{} '.format(w, len(W) - i)
-    result += '{:+.2f}'.format(b[0])
-    return result
-
-
-def get_batch(batch_size=32):
-    """Builds a batch i.e. (x, f(x)) pair."""
-    random = torch.Tensor(np.linspace(0, 1, 32))
-    x = make_features(random)
-    y = f(x)
-    return x, y
+def train(args, model, device, train_loader, optimizer, epoch):
+    def closure():
+        optimizer.zero_grad()
 
 
-# Define model
-fc = torch.nn.Linear(W_target.size(0), 1)
-fc.load_state_dict(torch.load('fc.sv'))
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            if batch_idx % args.log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.item()))
+    model.train()
 
-batch_x, batch_y = get_batch()
-op = optim.ARGD1(fc.parameters(), lr=0.1)
-
-
-loss = 0.0
-
-def closure():
-    global loss
-    global output
-    fc.zero_grad()
-
-    output = F.smooth_l1_loss(fc(batch_x), batch_y)
-    loss = output.item()
-
-    output.backward()
-
-while True:
     closure()
 
-    op.step(closure)
+    optimizer.step(closure)
 
-    if op.state['n_iter'] % 1000 == 0:
-        print(loss)
+def test(args, model, device, test_loader, result):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-    if loss < 1e-5:
-        break
+    test_loss /= len(test_loader.dataset)
 
-print('==> Iterations {}'.format(op.state['n_iter']))
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+    result.append(correct)
+
+def main():
+    # Training settings
 
 
-print('==> Learned function:\t' + poly_desc(fc.weight.view(-1), fc.bias))
-print('==> Actual function:\t' + poly_desc(W_target.view(-1), b_target))
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--optim', default='SGD', help='Optimiser to use (default: SGD)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    use_cuda = False #not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device('cuda' if use_cuda else 'cpu')
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('./data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('./data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+
+    model = Net()
+    model.load_state_dict(torch.load('init.model'))
+    model.to(device)
+    try:
+        optim_class = getattr(optim, args.optim)
+        optimizer = optim_class(model.parameters(), lr=args.lr)
+    except Exception as e:
+        print(e)
+        raise ValueError('Undefined Optimiser: {}'.format(args.optim))
+
+    result = []
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader, result)
+
+    with open('{}.result'.format(args.optim), 'wb') as f:
+        dill.dump((len(test_loader.dataset), result), f)
+
+    torch.save(model.state_dict(), '{}.model'.format(args.optim))
+
+if __name__ == '__main__':
+    main()
