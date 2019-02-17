a79a37a96dfdb834e184de8518d72427d709a012
Titus Pinta <titus.pinta@gmail.com>
2019-02-17 21:49:03 +0200

    Better viewer

--- viewer/viewer.py
+++ viewer/viewer.py
@@ -11,26 +11,32 @@ import main
 
 
 def help():
+    print('\nthe following functions and variables are available:\n')
     print('use_cuda:')
     print('\tboolean variable default is True if cuda is available')
-    print('\tcan be modefied directlly')
+    print('\tcan be modified directly')
     print('\nexample modification viewer.use_cuda=False')
-    print('\nthe following functions are available:\n')
     print('load_model(path)')
     print('\treturns the model stored in the specified path')
     print('\tplease provide the extension (normally .model or .model.part)')
     print('\nexample call: viewer.load_model(\'SGD.model\')\n')
     print('load result(path)')
-    print('\trerturns the result vector stored in the specified path')
+    print('\treturns the result vector stored in the specified path')
     print('\tplease also provide the extension')
-    print('\tthe result is a tupe with the first component the number of testing images')
-    print('\t\tand the second component a vector with tuples composed of the number')
-    print('\t\tof correct predictions and the loss for each epoch')
+    print('\tthe result is a tupel with the first component the number of testing images')
+    print('\t\tand the second component a tuple with the params (lr),\n\t\tthrd component a vector'+
+          ' with the number of correct predictions\n\t\tand the forth a vector with the loss for each' +
+          ' epoch')
     print('\nexample call: viewer.load_result(\'SGD.result\')\n')
     print('load_mnist(batch_size=64, test_batch_size=1000, use_cuda=True, seed=1)')
     print('\tthe meaning of the parameters is the same as for the torch dataloader')
     print('\nexample call: viewer.load_mnist()\n')
-
+    print('plot_result(result, plot=None, loss=False, correct=True, fraction=True, show=True)')
+    print('\tplots the result info and returns the matplotlib object')
+    print('\t\tplot=a previously constructed plot with which to combine the current plot')
+    print('\t\tloss=True if the plot should contain the loss, False for the number of correct predictions')
+    print('\t\tcorrect=True for num of correct prediction false for number of wrong predictions')
+    print('\t\tfraction=True divides num correct by num total')
 use_cuda = torch.cuda.is_available()
 
 
@@ -77,20 +83,26 @@ def plot_result(result, plot=None, loss=False, correct=True, fraction=True, show
     if plot is not None:
         plt = plot
 
-    if correct:
-        data = np.array(result[1])
+    if loss:
+        data = np.array(result[3])
+        ymax = np.max(data) + 0.1
+
     else:
-        data = result[0] - np.array(result[1])
+        if correct:
+            data = np.array(result[2])
+        else:
+            data = result[0] - np.array(result[2])
 
-    ymax = result[0]
+        ymax = result[0]
 
-    if fraction:
-        data = data / result[0]
-        ymax = 1
-    epochs = np.linspace(0, len(result[1]), len(result[1]))
+        if fraction:
+            data = data / result[0]
+            ymax = 1
+
+    epochs = np.linspace(0, len(result[2]), len(result[2]))
 
     plt.ylim(0, ymax)
-
+    print(data)
     plt.plot(epochs, data)
     if show:
         plt.show()
